8c02806b419bb2583844ae82f0943c5c23bc3f40
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2021-02-18 17:55:39 +0100

    jQuery training

--- src/Zadania/Dzien_4/3_Eventy/js/app.js
+++ src/Zadania/Dzien_4/3_Eventy/js/app.js
@@ -1,137 +1,78 @@
-// $(function () {
-//     $('a').on('click', function (event) {
-//         event.preventDefault();
-//         console.log('click');
-//     });
-//
-//     $('.menu').find('a').on('click', function () {
-//         console.log("menu find a onclic function");
-//     });
-//
-//     $('.menu').find('a').off('click');
-//
-//     $('.hero-buttons').find('.ironMan-btn').one('click', function () {
-//         alert("you clicked me")
-//     });
-//
-//     const buttons = $(".hero-buttons").find("button");
-//     const first = buttons.eq(0);
-//     const second = buttons.eq(1);
-//     const third = buttons.eq(2);
-//
-//     first.on('click', () => {
-//         console.log("kliknięto mnie")
-//     });
-//
-//     second.one('click', () => {
-//         console.log("kliknięto mnie ale już drugi raz nie dam się kliknąć")
-//     });
-//
-//     third.on('click', () => {
-//         first.off();
-//         second.off();
-//     });
-//
-//
-//     const superHeroDesc = $(".superhero-description");
-//     superHeroDesc.find("dd").hide();
-//     superHeroDesc.find("dt").on("click", function () {
-//         const dd = $(this).next()
-//         if(dd.is(":visible")){
-//             dd.hide();
-//         } else {
-//             dd.show();
-//         }
-//     });
-//
-//     const loginForm = $(".login");
-//     loginForm.find(".show-hide-btn").on('click', function (event) {
-//         event.preventDefault();
-//         console.log("działam");
-//
-//         const pswd = $(this).prev();
-//         const type = pswd.attr("type");
-//
-//         if(type === "password"){
-//             pswd.attr("type", "text")
-//         } else {
-//             pswd.attr("type", "password")
-//         }
-//     });
-//
-//     $(".menu").on("mouseenter", function () {
-//         console.log("Hura")
-//     });
-//
-//     const loginF = $("form.login").find("input");
-//     loginF.on('focus', function () {
-//         $(this).css("background-color", "white")
-//         $(this).css("box-shadow", "0px 0px 5px 5px green");
-//     });
-//
-//
-//     loginF.on('blur', function () {
-//         $(this).css("box-shadow", "");
-//         $(this).css("background-color", "grey");
-//
-//     })
-// });
 $(function () {
-    let buttons = $('.hero-buttons').find("button");
-    console.log(buttons)
+    $('a').on('click', function (event) {
+        event.preventDefault();
+        console.log('click');
+    });
 
-    let button1 = buttons.eq(0);
-    let button2 = buttons.eq(1);
-    let button3 = buttons.eq(2);
+    $('.menu').find('a').on('click', function () {
+        console.log("menu find a onclic function");
+    });
 
-    console.log(button1)
-    console.log(button2)
-    console.log(button3)
+    $('.menu').find('a').off('click');
 
-    button1.on('click', function () {
-        console.log('Klilnieto mnie')
-    })
-    button2.one('click', function () {
-        console.log('Kliknieto mnie ale juz drugi raz nie dam sie kliknac')
-    })
+    $('.hero-buttons').find('.ironMan-btn').one('click', function () {
+        alert("you clicked me")
+    });
 
-    button3.on('click', function (e) {
-        button1.off();
-        button2.off();
-    })
+    const buttons = $(".hero-buttons").find("button");
+    const first = buttons.eq(0);
+    const second = buttons.eq(1);
+    const third = buttons.eq(2);
 
-    let superHeroDescription = $('.superhero-description');
-    console.log(superHeroDescription)
-    let ddElements = superHeroDescription.find('dd')
-    ddElements.hide();
+    first.on('click', () => {
+        console.log("kliknięto mnie")
+    });
 
-    let dtElements = superHeroDescription.find('dt');
+    second.one('click', () => {
+        console.log("kliknięto mnie ale już drugi raz nie dam się kliknąć")
+    });
 
-    dtElements.on('click', function () {
-        const dd = $(this).next();
+    third.on('click', () => {
+        first.off();
+        second.off();
+    });
 
-        if (dd.is(":visible")) {
+
+    const superHeroDesc = $(".superhero-description");
+    superHeroDesc.find("dd").hide();
+    superHeroDesc.find("dt").on("click", function () {
+        const dd = $(this).next()
+        if(dd.is(":visible")){
             dd.hide();
         } else {
             dd.show();
         }
-    })
+    });
 
-    let login = $('.login');
-    let button = login.find('.show-hide-btn');
-    console.log(login)
-    console.log(button)
-
-    button.on('click', function (e) {
-        e.preventDefault();
+    const loginForm = $(".login");
+    loginForm.find(".show-hide-btn").on('click', function (event) {
+        event.preventDefault();
+        console.log("działam");
 
         const pswd = $(this).prev();
-        const type = pswd.attr('type');
+        const type = pswd.attr("type");
 
-        if (type === "password") {
-            pswd.attr('type', 'text')
+        if(type === "password"){
+            pswd.attr("type", "text")
         } else {
-            pswd.attr('type', 'password')
+            pswd.attr("type", "password")
         }
+    });
+
+    $(".menu").on("mouseenter", function () {
+        console.log("Hura")
+    });
+
+    const loginF = $("form.login").find("input");
+    loginF.on('focus', function () {
+        $(this).css("background-color", "white")
+        $(this).css("box-shadow", "0px 0px 5px 5px green");
+    });
+
+
+    loginF.on('blur', function () {
+        $(this).css("box-shadow", "");
+        $(this).css("background-color", "grey");
+
     })
 });
